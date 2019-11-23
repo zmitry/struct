73bf045f42fd906b118b31a25bbb11ce514abba1
dmitry <kraken@live.ru>
2019-11-23 12:30:57 +0200

    tests fix

--- src/pairing-heap-simple.test.ts
+++ src/pairing-heap-simple.test.ts
@@ -1,19 +1,18 @@
-import { createHeap, toString, MergePass } from './pairing-heap-simple';
+import { createHeap } from './pairing-heap-simple';
 // TODO add more cases
 test('basic', () => {
   const h = createHeap();
-  h.push(6);
-  h.push(1);
-  h.push(1);
   h.push(2);
   h.push(4);
   h.push(3);
+  h.push(1);
   h.push(5);
-  console.log(toString(h.root), h.root);
-  const r = MergePass(h.root?.left as any);
-  console.log(toString(r));
-  const r2 = MergePass(r as any);
-  console.log(toString(r2));
+  h.push(6);
+  h.push(1);
+  expect(h.top()).toBe(1);
+  h.pop();
+  h.pop();
+  expect(h.top()).toBe(2);
 }, 1000);
 
 // test.only('child', () => {
